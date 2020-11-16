965b46dc22cecae3c607dce2a122ec6d47535cf3
Robin Paul <robinpaul288@gmail.com>
2020-11-17 00:48:21 +0530

    Created patients dashboard login page edited controller

--- src/main/java/com/doctor/controller/AdminController.java
+++ src/main/java/com/doctor/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.doctor.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +11,15 @@ public class AdminController {
 	
 	@GetMapping("/registerPatients")
 	public String patientRegisterPage() {
-		return "register_patient";
+		return "patients/register_patient";
+	}
+	@PostMapping("/login")
+	public String loginPage() {
+		return "patients/login";
+	}
+	
+	@PostMapping("/loginPatient")
+	public String patientDashBoard() {
+		return "/patients/home";
 	}
 }
